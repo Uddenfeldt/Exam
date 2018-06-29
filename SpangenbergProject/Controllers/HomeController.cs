cb2787436ec4d2fd04665b34f8ff7310303ef8fc
Uddenfeldt <Marcuddenfeldt@hotmail.com>
2018-06-29 15:27:22 +0200

    first commit

---
+++ SpangenbergProject/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpangenbergProject.Models;
+
+namespace SpangenbergProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+        public IActionResult ForCompany()
+        {
+            return View();
+        }
+        public IActionResult ForPrivate()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
