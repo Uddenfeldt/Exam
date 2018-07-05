b6039fc4326195bef241b19acf5c2b5488d4c53e
Uddenfeldt <Marcuddenfeldt@hotmail.com>
2018-07-05 14:54:37 +0200

    Added booking site and a few other pages

---
+++ SpangenbergProject/Controllers/Company/CompanyController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SpangenbergProject.Controllers.Company
+{
+    public class CompanyController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Leadership()
+        {
+            return View();
+        }
+        public IActionResult GroupAndTeam()
+        {
+            return View();
+        }
+
+        public IActionResult Organization()
+        {
+            return View();
+        }
+
+        public IActionResult Conflicts()
+        {
+            return View();
+        }
+    }
+}
--- SpangenbergProject/Models/CustomerContact/CustomerContactController.cs
+++ SpangenbergProject/Models/CustomerContact/CustomerContactController.cs
@@ -1,25 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace SpangenbergProject.Models.Customer
+namespace SpangenbergProject.Models.CustomerContact
 {
-    public class CustomerModel : Controller
+    public class CustomerContactController : Controller
     {
-        public int ID { get; set; }
         public string firstName { get; set; }
         public string lastName { get; set; }
-        [Required]
-        [MinLength(10)]
-        [MaxLength(12)]
-        public int personalNumber { get; set; }
+        public string company { get; set; }
         public string email { get; set; }
-        public DateTime dtm { get; set; }
-        public DateTime tme { get; set; }
+        public string message { get; set; }
     }
 }
